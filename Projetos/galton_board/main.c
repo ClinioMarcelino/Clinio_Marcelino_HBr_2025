3bb141e8905791ad7edcc8c0f9edca094d68d59d
Clinio Marcelino <cliniof@gmail.com>
2025-04-21 12:30:36 -0300

    tests

--- Projetos/galton_board/main.c
+++ Projetos/galton_board/main.c
@@ -2,8 +2,10 @@
 #include <math.h>
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
+#include "hardware/adc.h"
 #include "inc/ssd1306.h"
 #include <string.h>
+#include <time.h>
 
 #define I2C_SDA 14
 #define I2C_SCL 15
@@ -12,6 +14,21 @@
 #define PINS_SPACE 4
 #define N_LINHAS 10
 #define Y_INICIAL_BOARD 5
+#define MOVEMENT 2
+
+struct Bola{
+    uint8_t x,y;
+};
+
+uint8_t g_board_pins_local[128][64] = {0};
+
+void init_random() {
+    adc_init();
+    adc_select_input(0); // suponha que o ADC0 esteja livre
+
+    uint16_t noise = adc_read(); // Leitura de ruído do ADC
+    srand(noise); // Usa o ruído como semente
+}
 
 void init_i2c(){
    
@@ -41,12 +58,42 @@ void draw_pins(uint8_t *ssd, struct render_area *frame_area, int8_t n_linhas){
     }
 }
 
-uint8_t g_board_pins_local[128][64] = {0};
+// returns 0 for Left and 1 for Right
+int left_or_right(){
+    int a = rand();
+    int b = rand();
+
+    // Uma operação simples para combinar a e b, mas garantindo uma boa distribuição
+    int combined = (a * 31 + b * 17) % 100; // Modifique os coeficientes conforme necessário
+
+    return combined % 2; // Retorna 0 ou 1
+}
+
+void update_ball_position(struct Bola *bola){
+    if(((bola->y) - Y_INICIAL_BOARD) % 4 <= 1 && (bola->y >=4) && (bola->y <= (N_LINHAS*PINS_SPACE))){
+        if(left_or_right()){
+            bola->x += MOVEMENT;
+        }
+        else
+            bola->x -= MOVEMENT;
+    }
+    bola->y += MOVEMENT;
+
+    if (bola->y > 50){
+        bola->y = 0;
+        bola->x = ssd1306_width / 2;
+    }
+}
+
+void position_ball(uint8_t *ssd, struct Bola *bola){
+    ssd1306_set_pixel(ssd, bola->x, bola->y,1);
+}
 
 int main(){
     stdio_init_all();
     
     init_i2c();
+    init_random();
 
     struct render_area frame_area = {
         start_column : 0,
@@ -60,55 +107,24 @@ int main(){
     memset(ssd, 0 , ssd1306_buffer_length);
     render_on_display(ssd, &frame_area);
 
-    
-
     char str[32];
 
-    int y=0;
-    int z=10;
+    struct Bola ball = {
+        x:ssd1306_width/2,
+        y:0
+    };
+
    
     while (true) {
         memset(ssd, 0 , ssd1306_buffer_length);
         draw_pins(ssd,&frame_area,10);
-        ssd1306_set_pixel(ssd,64,y,1);
-        // ssd1306_set_pixel(ssd,64,z,1);
-
         
-        
-
-
-
-        
+        position_ball(ssd, &ball);
+          
         render_on_display(ssd,&frame_area);
 
-        if(y>50)
-            y=0;
-        else
-            y+=2;
-        
-
-            sleep_ms(100);
+        update_ball_position(&ball);
+        sleep_ms(250);
     }
 
-    // int d,e = 0;
-
-    // while (true) {
-    //     char r = rand() % 2;
-
-    //     if(r)
-    //         d++;
-    //     else
-    //         e++;
-        
-    //     memset(ssd, 0 , ssd1306_buffer_length);
-    //     snprintf(str, sizeof(str), "%d .", r);
-    //     ssd1306_draw_string(ssd, 5, 20, str);
-
-    //     snprintf(str, sizeof(str), "D %d  E  %d", d,e);
-    //     ssd1306_draw_string(ssd, 5,50, str);
-
-    //     render_on_display(ssd, &frame_area);
-
-    //     sleep_ms(100);
-    // }
 }
